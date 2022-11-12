ac19929c941cd10122ec8a1c5bbf099af2e7a1fb
Андрій Петруняк <hasherama8@gmail.com>
2022-11-12 22:11:20 +0200

    created hash and shifr functions

--- config.js
+++ config.js
@@ -2,9 +2,11 @@ const port = 5555;
 const pgConnectionHost = 'localhost';
 const pgConnectionPort = 5080;
 const pgConnectionUser = 'library';
-const pgConnectionPassword = 'library'
+const pgConnectionPassword = 'library';
 const pgConnectionDB = 'library_of_books_DB';
-
+const cipherAlgorithm = 'aes-256-cbc';
+const secret = 'a6c22b3a4154cefe165baea2729c2f616c472567741a276ecaf7206e30e61d9c';
 module.exports = { port, pgConnectionHost,
-    pgConnectionPort, pgConnectionUser, pgConnectionPassword, pgConnectionDB
+    pgConnectionPort, pgConnectionUser, pgConnectionPassword, pgConnectionDB,
+    cipherAlgorithm, secret
 }
---
+++ src/crypto/crypto.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const { cipherAlgorithm, secret } = require('../../config');
+const argon2 = require('argon2');
+
+const key = Buffer.from(secret, 'hex');
+
+const encryptData = async (data) => {
+    const iv = crypto.randomBytes(16).toString('hex').slice(0, 16);
+    const cipher = crypto.createCipheriv(cipherAlgorithm, key, iv);
+
+    let encrypted = cipher.update(String(data), 'utf8', 'hex')
+    encrypted += cipher.final('hex');
+
+    return `${encrypted}:${iv}`
+}
+
+const decrypto = async (encryptedData) => {
+    const [encryptedString, iv] = encryptedData.split(':');
+    const decipher = crypto.createDecipheriv(cipherAlgorithm, key, iv)
+
+    let decrypted = decipher.update(encryptedString, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+
+    return decrypted
+}
+
+const createHash = async (data) => {
+    const hash = await argon2.hash(data)
+    return hash;
+}
+
+const compareHash = async (hash, data) => {
+    const compareResult = await argon2.verify(hash, data);
+    return compareResult
+}
+
+module.exports = { encryptData, createHash, decrypto, compareHash }
