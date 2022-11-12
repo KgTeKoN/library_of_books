eaec628d6481302b09f32b2bea8ad7b0899223e4
Андрій Петруняк <hasherama8@gmail.com>
2022-11-12 21:52:17 +0200

    replace jwt function to correct file

--- src/authorization/validate.inputData.js
+++ src/authorization/validate.inputData.js
@@ -1,8 +1,1 @@
-const jwt = require('jsonwebtoken');
 
-const token = async (data, secret, time) => {
-    const token = await jwt.sign(data, secret, {expiresIn: time});
-    return token;
-}
-
-module.exports = { token }
