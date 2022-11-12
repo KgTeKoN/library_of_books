36de0ab5ebee2d552571496f2eec6f47c9152a75
Андрій Петруняк <hasherama8@gmail.com>
2022-11-12 21:53:45 +0200

    created validator for authorization

--- src/authorization/validate.inputData.js
+++ src/authorization/validate.inputData.js
@@ -1,1 +1,25 @@
+const { signUpSchema, signInSchema } = require('./validate.schema');
 
+const signUpValidator = async (req, res, next) => {
+    try {
+        await signUpSchema.validateAsync(req.body);
+        return next();
+    } catch (e) {
+        const result = e.details[0].message;
+        res.status(400).send(result);
+        res.end();
+    }
+}
+
+const signInValidator = async (req, res, next) => {
+    try {
+        await signInSchema.validateAsync(req.body);
+        return next();
+    } catch (e) {
+        const result = e.details[0].message;
+        res.status(400).send(result);
+        res.end();
+    }
+};
+
+module.exports = { signUpValidator, signInValidator };
